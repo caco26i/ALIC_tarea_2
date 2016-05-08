2714ffcce59902fc61bf6440666e5007ca0d8195
caco26i <isaac_antonio123@hotmail.com>
2016-05-07 23:27:45 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def onButtonPressed(self, button):
+        print("Hello World!")
+
+builder = Gtk.Builder()
+builder.add_from_file("example.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("main_window")
+window.show_all()
+
+Gtk.main()
