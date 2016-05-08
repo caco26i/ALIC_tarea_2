577cf4f58a82af14dcecdb7b08da25a810adcd73
caco26i <isaac_antonio123@hotmail.com>
2016-05-08 12:52:52 -0600

    se agrega el metodo de obtener datos del combobox, el boton siguiente muestra el siguiente tab

--- test.py
+++ test.py
@@ -11,8 +11,29 @@ class Handler:
 
     def app_1_1_siguiente(self, button):
         print("siguiente ")
-        builder.get_object(name)
+        notebook_app_1 = builder.get_object("notebook_app_1")
+        
+        combobox_tipo_vector = builder.get_object("combobox_tipo_vector")
+        combobox_tipo_vector_value = self.get_combo_value(combobox_tipo_vector)
 
+        combobox_cantidad_vectores = builder.get_object("combobox_cantidad_vectores")
+        combobox_cantidad_vectores_value = self.get_combo_value(combobox_cantidad_vectores)
+
+        print(combobox_tipo_vector_value)
+        print(combobox_cantidad_vectores_value)
+
+        notebook_app_1.next_page()
+
+    ##############################
+    #    GETTERS AND SETTERS
+    ##############################
+    def get_combo_value(self, combo):
+        tree_iter = combo.get_active_iter()
+        if tree_iter != None:
+            model = combo.get_model()
+            return model[tree_iter][0]
+
+        
 builder = Gtk.Builder()
 builder.add_from_file("example.glade")
 builder.connect_signals(Handler())
