d619e25e5405064c51ee2a289b63e4bbd070a86f
caco26i <isaac_antonio123@hotmail.com>
2016-05-08 16:59:50 -0600

    se crean los atributos de tamaños de vectores y cantidad de elementos

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import numpy as np
+#import numpy as np
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Handler:
+
+    cantidad_vectores_app_1 = None
+    cantidad_elementos_vector_app_1 = None
 
     def app_1_1_siguiente(self, button):
         print("siguiente")
@@ -19,23 +22,23 @@ class Handler:
         print(combobox_tipo_vector_value)
         print(combobox_cantidad_vectores_value)
 
-        if not combobox_tipo_vector_value is None and not combobox_cantidad_vectores_value is None:
-            cantidad_vectores = combobox_cantidad_vectores_value
+        if combobox_tipo_vector_value is not None and combobox_cantidad_vectores_value is not None:
+            self.cantidad_vectores_app_1 = combobox_cantidad_vectores_value
             if combobox_tipo_vector_value == "R":
-                cantidad_elementos_vector = 1
+                self.cantidad_elementos_vector_app_1 = 1
             elif combobox_tipo_vector_value == "R2":
-                cantidad_elementos_vector = 2
+                self.cantidad_elementos_vector_app_1 = 2
             elif combobox_tipo_vector_value == "R3":
-                cantidad_elementos_vector = 3
+                self.cantidad_elementos_vector_app_1 = 3
 
             for i in range(0, 5):
                 vector = builder.get_object("hbox_vector_"+str(i+1))
-                vector_visible = i+1 <= cantidad_vectores
+                vector_visible = i+1 <= self.cantidad_vectores_app_1
                 vector.set_visible(vector_visible);
                 if vector_visible:
                     for j in range(0, 3):
                         field = builder.get_object("entry_vector_"+str(i+1)+"_"+str(j+1))
-                        field_visible = j+1 <= cantidad_elementos_vector
+                        field_visible = j+1 <= self.cantidad_elementos_vector_app_1
                         field.set_visible(field_visible);
 
             notebook_app_1.next_page()
@@ -45,30 +48,12 @@ class Handler:
 
         notebook_app_1 = builder.get_object("notebook_app_1")
 
-        combobox_tipo_vector = builder.get_object("combobox_tipo_vector")
-        combobox_tipo_vector_value = self.get_combo_value(combobox_tipo_vector)
-
-        combobox_cantidad_vectores = builder.get_object("combobox_cantidad_vectores")
-        combobox_cantidad_vectores_value = self.get_combo_value(combobox_cantidad_vectores)
-
-        print(combobox_tipo_vector_value)
-        print(combobox_cantidad_vectores_value)
-
         matriz_elementos = []
 
-        if not combobox_tipo_vector_value is None and not combobox_cantidad_vectores_value is None:
-            cantidad_vectores = combobox_cantidad_vectores_value
-            if combobox_tipo_vector_value == "R":
-                cantidad_elementos_vector = 1
-            elif combobox_tipo_vector_value == "R2":
-                cantidad_elementos_vector = 2
-            elif combobox_tipo_vector_value == "R3":
-                cantidad_elementos_vector = 3
-
-            for i in range(0, cantidad_vectores):
+        if self.cantidad_vectores_app_1 is not None and self.cantidad_elementos_vector_app_1 is not None:
+            for i in range(0, self.cantidad_vectores_app_1):
                 temp_list = []
-                for j in range(0, cantidad_elementos_vector):
-
+                for j in range(0, self.cantidad_elementos_vector_app_1):
                     field = builder.get_object("entry_vector_"+str(i+1)+"_"+str(j+1))
                     value_field = int(field.get_text())
                     temp_list.append(value_field)
