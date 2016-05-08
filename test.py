b2b6c92836586ba01862e7d920449a47e0ad4370
caco26i <isaac_antonio123@hotmail.com>
2016-05-08 00:26:22 -0600

    se agregan las listas de cantidad de vectores y tipos

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ class Handler:
     def onButtonPressed(self, button):
         print("Hello World!")
 
+    def home_siguiente(self, tabla):
+        print("siguiente " + tabla)
+
 builder = Gtk.Builder()
 builder.add_from_file("example.glade")
 builder.connect_signals(Handler())
