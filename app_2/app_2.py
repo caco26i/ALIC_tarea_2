75c3c8e427e53a7cfc5c8ac88c1fe5f0dd85921a
Federico <fedemonte.95@gmail.com>
2016-05-11 22:52:11 -0600

    nuevas adiciones

--- app_2/app_2.py
+++ app_2/app_2.py
@@ -1,4 +1,26 @@
+import gi
+import sys
 from gi.repository import Gtk
 import numpy as np
-import gi
 gi.require_version('Gtk', '3.0')
+
+
+class App2:
+    def __init__(self):
+        self.glade_file = "view_app_4.glade"
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(self.glade_file)
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("main_window")
+
+        self.window.show_all()
+
+    def on_window_destroy(self, object, data=None):
+        print("quit with cancel")
+        Gtk.main_quit()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main = App2()
+    Gtk.main()
